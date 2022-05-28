ff057aef0669c6ccaa867b3b35d6643560020512
Michael <michael138leighton@gmail.com>
2022-05-28 13:48:23 -0500

    chat component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatInboxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/chat-inbox/chat-inbox.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import io from 'socket.io-client';
+
+const SOCKET_ENDPOINT = 'localhost:3000';
+
+@Component({
+  selector: 'app-chat-inbox',
+  templateUrl: './chat-inbox.component.html',
+  styleUrls: ['./chat-inbox.component.scss']
+})
+export class ChatInboxComponent implements OnInit {
+  socket;
+  constructor() { }
+
+  ngOnInit() {
+    this.setupSocketConnection();
+  }
+
+  setupSocketConnection(){
+    this.socket = io(SOCKET_ENDPOINT)
+  }
+}
