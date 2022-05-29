ffa52fb2408e58d06bac044e44f57ace8df0c927
Michael <michael138leighton@gmail.com>
2022-05-28 19:10:04 -0500

    messages can now be added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
 
+import { FormsModule } from '@angular/forms'; 
+
 @NgModule({
   declarations: [
     AppComponent,
     ChatInboxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/chat-inbox/chat-inbox.component.ts
+++ src/app/chat-inbox/chat-inbox.component.ts
@@ -9,14 +9,31 @@ const SOCKET_ENDPOINT = 'localhost:3000';
   styleUrls: ['./chat-inbox.component.scss']
 })
 export class ChatInboxComponent implements OnInit {
+
   socket;
+  message: string;
+  messageList = [];
+
   constructor() { }
 
   ngOnInit() {
     this.setupSocketConnection();
   }
 
-  setupSocketConnection(){
-    this.socket = io(SOCKET_ENDPOINT)
-  }
+  setupSocketConnection() {
+    this.socket = io(SOCKET_ENDPOINT);
+    this.socket.on('message-broadcast', (data: string) => {
+    if (data) {
+    this.messageList.push(data)
+      }
+   });
+ }
+
+ SendMessage() {
+  this.socket.emit('message', this.message);
+  this.messageList.push(this.message)
+console.log(this.messageList)
+
+  this.message = '';
+}
 }
